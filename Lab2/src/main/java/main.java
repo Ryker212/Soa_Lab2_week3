92d1a9e9262c221d339b6a8179156d327f40aa1a
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-10 16:22:13 +0700

    สร้างmain

---
+++ Lab2/src/main/java/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer customer = new Customer();
+		 customer.setName("Jane Doe");
+		 customer.getPhoneNumbers().add(new
+		PhoneNumber("12345"));
+		 customer.getPhoneNumbers().add(new
+		PhoneNumber("222"));
+
+	}
+
+}
