265dd311dd0185146d5e87d9523ac1d9e5108c5b
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-10 16:14:54 +0700

    สร้างconstructor ให้Customer
    สร้างconstructor ให้Customer
    -มี Phonenumberเป็น object เป็นArrayList

--- Lab2/src/main/java/Customer.java
+++ Lab2/src/main/java/Customer.java
@@ -1,5 +1,14 @@
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 
 public class Customer {
 	private String name;
+	private ArrayList<PhoneNumber> phone;
 	
+	public Customer() {
+		
+		this.phone = new ArrayList();
+	}
+
 }
+
