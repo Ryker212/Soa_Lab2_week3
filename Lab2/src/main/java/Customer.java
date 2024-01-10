794ad3a5c99016894802afcfb2c6564421a535bf
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-10 16:27:05 +0700

    เพิ่มdependency สร้างmethod
    เพิ่มdependency สร้างmethod
    -get set

--- Lab2/src/main/java/Customer.java
+++ Lab2/src/main/java/Customer.java
@@ -1,14 +1,37 @@
-import java.lang.reflect.Constructor;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement; 
+
+@XmlRootElement
 public class Customer {
 	private String name;
 	private ArrayList<PhoneNumber> phone;
 	
 	public Customer() {
-		
 		this.phone = new ArrayList();
+	}
+	public Customer(String name) {
+		this.name =name;
+		this.phone = new ArrayList();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@XmlElement(name="phone-number")
+	public ArrayList<PhoneNumber> getPhoneNumbers() {
+		return this.phone;
+	}
+	
+	public void setPhoneNumbers(ArrayList<PhoneNumber> phoneNumbers) {
+		this.phone = phoneNumbers;
 	}
 
 }
-
